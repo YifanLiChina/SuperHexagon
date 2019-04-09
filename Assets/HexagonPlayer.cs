4db006014d3ac188188e9a9cc73f4915ab5019fd
yifanli <nanak12138@gmail.com>
2019-04-09 20:24:32 +0100

    Mainbody of Hexagon

---
+++ Assets/HexagonPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HexagonPlayer : MonoBehaviour {
+
+	public float moveSpeed = 600f;
+
+	float m_movement = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		m_movement = Input.GetAxisRaw("Horizontal");
+	}
+
+	void FixedUpdate(){
+		Debug.Log(m_movement);
+		transform.RotateAround(Vector3.zero, Vector3.forward, m_movement * Time.fixedDeltaTime * -moveSpeed);
+	}
+
+	void OnTriggerEnter2D(Collider2D collision){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
